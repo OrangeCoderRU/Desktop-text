e5bb09aae11a40570406f22823efcfd44120430e
OrangeCoderRU <max.sablin@mail.ru>
2019-11-17 17:31:11 +0300

    inserting code

---
+++ timer.py
@@ -0,0 +1,161 @@
+import numpy as np
+import ctypes
+from PIL import Image, ImageDraw, ImageFont
+import scipy.misc
+import os
+import datetime
+from win32api import GetSystemMetrics
+import json
+
+dict = {}
+
+def read_file():
+    with open("data_file.json", "r") as read_file: # read data file
+        dict = json.load(read_file)
+    return dict
+
+dict = read_file()
+
+def data_dump(dict):
+    date = datetime.datetime(int(dict.get("Год")), int(dict.get("Месяц")), int(dict.get("День")), 0, 0, 0)
+    today = datetime.datetime.now()
+    date = date - today #datetime.timedelta
+    string_day = " дней"
+    string_hour = " часов"
+    if ((date.days % 10) == 0) and (not (date.days % 10) == 10):
+        string_day = " дней"
+    elif (date.days) == 1:
+        string_day = " день"
+    elif (date.days) in range(10, 21, 1):
+        string_day = " дней"
+    elif (date.days % 10) in range(2, 5, 1):
+        string_day = " дня"
+    elif (date.days % 10) in range(5, 10, 1):
+        string_day = " дней"
+    if int((date.seconds / 3600)) == 21:
+        string_hour = " час"
+    elif int((date.seconds / 3600)) == 1:
+        string_hour = " час"
+    elif int((date.seconds / 3600)) in range(2, 5, 1):
+        string_hour = " часа"
+    elif int((date.seconds / 3600)) in range(22, 25, 1):
+        string_hour = " часа"
+    elif int((date.seconds / 3600)) in range(5, 10, 1):
+        string_hour = " чаcов"
+    elif int((date.seconds / 3600)) in range(10, 21, 1):
+        string_hour = " чаcов"
+    return str(date.days) + string_day + " " + str(int(date.seconds / 3600)) + string_hour + "\n"
+
+def create_image():
+    #определение разрешения экрана
+    monitor_width = GetSystemMetrics(0)
+    monitor_heitgh = GetSystemMetrics(1)
+
+    # создадим белое изображение
+    img = np.zeros((monitor_heitgh, monitor_width, 3), np.uint8)
+    img[:, :, :] = 255
+    return img
+
+def default_image():
+    #if dict["path"] == "Здесь пока ничего нет":
+        #img = Image.open(r'D:\Photo\IMG_4021-1920.jpg')
+    #D:\Photo\IMG_4021.jpg
+    #else:
+        #path = dict.get("path")
+        #img = Image.open(path)
+    #img = Image.open(r'D:\Photo\IMG_4021-1920.jpg')
+    #img.show()
+    path = dict.get("path")
+    img = Image.open(path)
+    arr = np.asarray(img, dtype='uint8')
+    return arr
+
+print(dict["status"])
+
+if dict["status"] == 0:
+    img = default_image()
+else:
+    img = create_image()
+
+#img = create_image()
+#img = default_image()
+
+def put_text_pil(img: np.array, txt: str):
+    im = Image.fromarray(img)
+
+    font_size = dict["font"]
+    font = ImageFont.truetype("ofont.ru_Clear Sans Thin.ttf", size=font_size)
+
+    draw = ImageDraw.Draw(im)
+    # здесь узнаем размеры сгенерированного блока текста
+    w, h = draw.textsize(txt, font=font)
+
+    draw = ImageDraw.Draw(im)
+    width, height = im.size
+    #x, y = (width-700, height-1050)
+    x, y = ((width / 1.5) + 150, height / 100)
+    #x, y = (width - 500, height - 600)
+    # теперь можно центрировать текст
+    #draw.text((int((img.shape[1] - w)/2) + 100, y_pos), txt, fill='rgb(0, 0, 0)', font=font)
+    draw.text((x, y), txt, fill='rgb(0, 0, 0)', font=font)
+    img = np.asarray(im)
+
+    return img
+
+def date_time():
+    date = datetime.datetime(2019, 11, 16, 0, 0, 0)
+    today = datetime.datetime.today()
+    date = date - today
+    return date
+
+def now_time():
+    today = datetime.datetime.now()
+    today_month = ""
+    if today.month == 1:
+        today_month = "января"
+    elif today.month == 2:
+        today_month = "февраля"
+    elif today.month == 3:
+        today_month = "марта"
+    elif today.month == 4:
+        today_month = "апреля"
+    elif today.month == 5:
+        today_month = "мая"
+    elif today.month == 6:
+        today_month = "июня"
+    elif today.month == 7:
+        today_month = "июля"
+    elif today.month == 8:
+        today_month = "августа"
+    elif today.month == 9:
+        today_month = "сентября"
+    elif today.month == 10:
+        today_month = "октября"
+    elif today.month == 11:
+        today_month = "ноября"
+    elif today.month == 12:
+        today_month = "декабря"
+    return str(today.day) + " " + today_month
+
+def set_wallpaper():
+    SPI_SETDESKWALLPAPER = 20 # константа, всегда = 20
+    p = os.path.abspath('my.jpg')
+    arg = ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, p , 0)
+    # SystemParametersInfoW - 64 bit системы
+    # SystemParametersInfoA - 32 bit системы
+
+
+task_text = " до " + str(dict['Задача'])
+text = " " + "Сегодня " + str(now_time()) + "\n" + " " + (str(data_dump(dict))) + task_text
+#sep = " " + (int(len(text) / 2)) * "-"
+
+#string = text + "\n" + sep
+#string_task = " Ты пидор " + "\n"
+
+#main_string = string + "\n" + string_task
+
+img = put_text_pil(img, text)
+image = Image.fromarray(img, 'RGB')
+image.save('my.jpg')
+
+set_wallpaper()
---
+++ timer_conf.py
@@ -0,0 +1,115 @@
+import json
+from tkinter import *
+from tkinter.ttk import Combobox
+from tkinter import messagebox
+from tkinter import filedialog as fd
+import subprocess
+import sys
+import os
+
+window = Tk()
+window.title('Desktop Text')
+window.geometry('600x200') # window size
+
+dict = {}
+
+lbl = Label(window, text="дней до...", font=("Arial Bold", 12))
+lbl.grid(row = 0, column = 0)
+ent = Entry(window, width=30, bd=1)
+ent.grid(row = 0, column = 1)
+
+lbl2 = Label(window, text="число", font=("Arial Bold", 12))
+lbl2.grid(row = 1, column = 0)
+
+lbl3 = Label(window, text="месяц", font=("Arial Bold", 12))
+lbl3.grid(row = 1, column = 1)
+
+lbl5 = Label(window, text="год", font=("Arial Bold", 12))
+lbl5.grid(row = 1, column = 2)
+
+lbl4 = Label(window, text="Задание:", font=("Arial Bold", 10), width=10)
+lbl4.grid(row = 4, column = 0)
+
+#lbl_data = Label(window, text="text", font=("Arial Bold", 12))
+#lbl_data.grid(row = 4, column = 1)
+task_listbox = Listbox(font = ('Arial Bold', 10), height = 4, width = 30)
+task_listbox.grid(row = 4, column = 1)
+
+dict_path = {}
+
+def insertFile():
+    file_name = fd.askopenfilename()
+    f = open(file_name)
+    p = os.path.abspath(file_name)
+    print(p)
+    #s = f.read()
+    #text.insert(1.0, s)
+    dict_path = {"path": p}
+    lbl5.configure(text = p)
+    f.close()
+
+def read_file():
+    with open("data_file.json", "r") as read_file: # read data file
+        dict = json.load(read_file)
+    for tasks in dict:
+        if tasks == "status":
+            break
+        else:
+            task_listbox.insert(END, str(tasks) + " - " + str(dict[tasks]))
+
+def clicked():
+    dict = {"Задача": ent.get(), "День": int(combo_days.get()), "Месяц": int(combo_month.get()), "Год": int(combo_year.get()), "status": state.get(), "path": lbl5.cget("text"), "font": int(combo_font.get())}
+    #dict.update(dict_path)
+    save_file(dict)
+    read_file()
+    program = "timer.py"
+    subprocess.Popen([sys.executable, program])
+
+def save_file(dict):
+    """Save data action"""
+    with open("data_file.json", "w") as write_file: # write data file
+        json.dump(dict, write_file, indent = 4)
+    messagebox.showinfo("Ok", "The data was successfully saved")
+
+read_file()
+
+state = IntVar()
+state.set(0)  # задайте проверку состояния чекбокса
+chk = Checkbutton(window, text='Белый фон', var=state)
+chk.grid(row = 3, column = 2)
+
+
+combo_days = Combobox(window, width=10)
+combo_days['values'] = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 29, 30, 31)
+combo_days.current(0)  # установите вариант по умолчанию
+combo_days.grid(row = 2, column = 0)
+
+combo_month = Combobox(window, width=10)
+combo_month['values'] = (1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11, 12)
+combo_month.current(0)  # установите вариант по умолчанию
+combo_month.grid(row = 2, column = 1)
+
+combo_year = Combobox(window, width=10)
+combo_year['values'] = (2019, 2020, 2021, 2022, 2023, 2024, 2025)
+combo_year.current(0)  # установите вариант по умолчанию
+combo_year.grid(row = 2, column = 2)
+
+lbl5 = Label(window, text="Размер шрифта:", font=("Arial", 10))
+lbl5.grid(row = 0, column = 2)
+
+combo_font = Combobox(window, width=10)
+combo_font['values'] = (10, 20, 30, 40, 50, 60, 70, 80, 90 , 100, 150, 200, 250, 300)
+combo_font.current(0)  # установите вариант по умолчанию
+combo_font.grid(row = 0, column = 3)
+
+btn = Button(window, text="Применить", command = clicked)
+btn.grid(row = 3, column = 1)
+
+btn_open = Button(text="Открыть", command=insertFile, width=25)
+btn_open.grid(row = 5, column = 2)
+
+lbl5 = Label(window, text="Здесь пока ничего нет", font=("Arial", 10))
+lbl5.grid(row = 4, column = 2)
+
+
+window.mainloop()
